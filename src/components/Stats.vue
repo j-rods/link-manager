118104e5958cc0d2afd9035debd2e86122b99e08
j-rods <jrrodriguez84@gmail.com>
2019-08-02 10:27:04 +0100

    import getters into stats component, map getter and run in content

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,13 +1,21 @@
 <template>
-    
+  <div class="stats">
+    <h1>Stats Component</h1>
+    <p>There are currently {{ countLinks }} links</p>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
-
-}
+  name: 'Stats',
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ]),
+  }
+};
 </script>
 
 <style>
-
 </style>
