2d490ae554fded43ef54b4768dbb3cdb2140f520
j-rods <jrrodriguez84@gmail.com>
2019-08-03 20:29:27 +0100

    create remove all links button, import actions, remove all function, styling

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,20 +2,50 @@
   <div class="stats">
     <h1>Stats Component</h1>
     <p>There are currently {{ countLinks }} links</p>
+    <button @click="removeAllLinks">Remove all links</button>
+    <p>{{ msg }}</p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations, mapActions } from 'vuex';
 export default {
   name: 'Stats',
+  data() {
+    return {
+      msg: '',
+    }
+  },
   computed: {
     ...mapGetters([
       'countLinks'
     ]),
+  },
+  methods: {
+    ...mapActions([
+      'removeAll'
+    ]),
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = 'All Links removed'
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+
   }
 };
 </script>
 
-<style>
+<style scoped>
+button {
+  padding: 10px;
+  margin-top: 30px;
+  width: 100%;
+  background: none;
+  border: 1px solid lightgrey;
+  outline: 0;
+  cursor: pointer;
+}
 </style>
